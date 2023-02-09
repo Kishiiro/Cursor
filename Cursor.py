f811edc438f0af659e8e51e429c0db915be1c4b5
Rolison Makilan D <isonmakilan@gmail.com>
2023-02-09 12:58:25 +0800

    Create Cursor.py

---
+++ Cursor.py
@@ -0,0 +1,11 @@
+import random
+import time
+import pyautogui as screen
+
+while True: 
+    x= random.randint(600,700)
+    y=random.randint(200,600)
+    screen.moveTo(x,y,0.5)
+    time.sleep(2)
+#making a stop thing
+      
