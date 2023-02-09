8f3bc4c8dd0f5ed958846a63005e5bcc9d96eafa
Rolison D. Makilan <isonmakilan@gmail.com>
2023-02-09 12:05:28 +0800

    first commit

---
+++ Practice/Cursor-BOT-not-Finished.py
@@ -0,0 +1,11 @@
+import pyautogui as screen
+import random
+import time
+
+while True: 
+    x= random.randint(600,700)
+    y=random.randint(200,600)
+    screen.moveTo(x,y,0.5)
+    time.sleep(2)
+#making a stop thing
+      
